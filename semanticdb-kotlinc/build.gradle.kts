21d241aa665f551df59bde31a9c99a62aaa6d311
Nicolas Guichard <nicolas.guichard@kdab.com>
2025-11-18 18:19:27 +0100

    Update protobuf to 4.33.1

--- semanticdb-kotlinc/build.gradle.kts
+++ semanticdb-kotlinc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     compileOnly(kotlin("compiler-embeddable"))
-    implementation("com.google.protobuf", "protobuf-java", "3.15.7")
+    implementation("com.google.protobuf:protobuf-java:4.33.1")
     implementation(projects.semanticdbKotlin)
 
     testImplementation(kotlin("compiler-embeddable"))
